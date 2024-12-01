87a4e7a71954eaf8efa4df5549bb953d22749bf1
sasavulic7 <62356252+sasavulic7@users.noreply.github.com>
2024-12-01 17:32:01 +0100

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,45 +1,35 @@
 import nodemailer from "nodemailer";
-import cors from "cors";
 
 // SMTP konfiguracija za Nodemailer
 const transporter = nodemailer.createTransport({
-  service: "gmail", // Možeš koristiti i druge SMTP servise (npr. Outlook, Yahoo)
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER, // Email adresa
     pass: process.env.EMAIL_PASS // Lozinka ili App Password
   }
 });
 
-// CORS middleware sa vašom produkcijskom adresom
-const corsHandler = cors({
-  origin: "crni-decak.vercel.app", // Vaš produkcijski URL
-});
-
 export default async function handler(req, res) {
-  return new Promise((resolve, reject) => {
-    corsHandler(req, res, async () => {
-      if (req.method === "POST") {
-        const { name, email, message } = req.body;
+  if (req.method === "POST") {
+    const { name, email, message } = req.body;
 
-        const mailOptions = {
-          from: email,
-          to: process.env.EMAIL_USER, // Tvoj email na koji ćeš primati poruke
-          subject: `Kontakt forma - poruka od ${name}`,
-          text: `Ime: ${name}\nEmail: ${email}\nPoruka:\n${message}`
-        };
+    const mailOptions = {
+      from: email,
+      to: process.env.EMAIL_USER, // Tvoj email na koji ćeš primati poruke
+      subject: `Kontakt forma - poruka od ${name}`,
+      text: `Ime: ${name}\nEmail: ${email}\nPoruka:\n${message}`
+    };
 
-        try {
-          // Slanje emaila
-          await transporter.sendMail(mailOptions);
-          res.status(200).json({ message: "Email je uspešno poslat!" });
-        } catch (error) {
-          console.error("Greška pri slanju emaila:", error);
-          res.status(500).json({ message: "Došlo je do greške pri slanju emaila." });
-        }
-      } else {
-        // Odbijanje drugih HTTP metoda osim POST
-        res.status(405).json({ message: "Method Not Allowed" });
-      }
-    });
-  });
+    try {
+      // Slanje emaila
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: "Email je uspešno poslat!" });
+    } catch (error) {
+      console.error("Greška pri slanju emaila:", error);
+      res.status(500).json({ message: "Došlo je do greške pri slanju emaila." });
+    }
+  } else {
+    // Odbijanje drugih HTTP metoda osim POST
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
 }
